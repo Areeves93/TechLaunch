ce659ef374bd2e45951a1d752fa29baed35469a3
areeves93 <areeves@techlaunch.io>
2017-10-19 19:17:47 -0700

    Test 2

--- test2/Q5.js
+++
@@ -1,5 +0,0 @@
-// Log to the console a Boolean expression that tests
-// whether two numbers are equal, and whose answer evaluates to false.
-
-console.log(10 > 9)
-console.log(15 > 35)
--- test2/Q6.js
+++
@@ -1,4 +0,0 @@
-// Log to the console a Boolean expression that
-// tests whether two numbers are not equal, and whose answer evaluates to true.
-
-console.log(2!=3);
